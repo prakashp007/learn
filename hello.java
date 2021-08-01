ad9be6610603c59a26c1d9a61eaf6da0741a489d
Urmil89 <60167367+urmil89@users.noreply.github.com>
2021-08-01 16:51:34 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 public class hello
 {
-	
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String name;
+		System.out.println("Enter your name");
+		name = s.nextLine();
+		System.out.println("your name is " + name);
 }
